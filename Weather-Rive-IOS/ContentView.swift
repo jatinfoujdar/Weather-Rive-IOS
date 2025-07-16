d7c9980e978a7ab21d86238b1f4e5ee0b76fac14
Jatin Foujdar <jatin321001@gmail.com>
2025-07-17 01:39:43 +0530

    Initial Commit

---
+++ Weather-Rive-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Weather-Rive-IOS
+//
+//  Created by jatin foujdar on 17/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Weather-Rive-IOS/Weather_Rive_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_Rive_IOSApp.swift
+//  Weather-Rive-IOS
+//
+//  Created by jatin foujdar on 17/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_Rive_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
