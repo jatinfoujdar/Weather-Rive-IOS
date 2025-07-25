7373e8c468a3f0a058c3c3e59979d66ea3834ba7
Jatin Foujdar <jatin321001@gmail.com>
2025-07-25 15:56:32 +0530

    segmented control commit

--- Weather-Rive-IOS/Views/Components/SegmentedControl.swift
+++ Weather-Rive-IOS/Views/Components/SegmentedControl.swift
@@ -37,6 +37,18 @@ struct SegmentedControl: View {
                 .blendMode(.overlay)
                 .shadow(color: .black.opacity(0.2), radius: 0 ,x: 0, y: 1)
                 .blendMode(.overlay)
+                .overlay{
+                    HStack {
+                        Divider()
+                            .frame(width: UIScreen.main.bounds.width / 2, height: 3)
+                            .background(LinearGradientColor.underline)
+                            .blendMode(.overlay)
+                        
+                    }
+                    .frame(maxWidth: .infinity, alignment: selection == 0 ? .leading : .trailing)
+                    .offset(y: -1)
+                    
+                }
         }
         .padding(.top,25)
     }
